8824a63950fb6df895542b7d6196dff424a8c3c9
akshaya2408 <akshayareddy0824@gmail.com>
2025-04-26 00:40:35 +0530

    AddStudent.js

--- client/my-app/src/components/AddStudent.js
+++ client/my-app/src/components/AddStudent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-function AddStudent({id}) {
+function AddStudent() {
     const [student, setStudent] = useState({
         name: '',
         email: '',
